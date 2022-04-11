95c1193515e58419e49e05f44a4486759aa81aa0
Wengel Awelachew <wenguawelachew@gmail.com>
2022-04-11 07:31:52 -0700

    How to print to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 /**
  * main - entry point
  * Read the argument
- * Return: 1 after printing the argument to the standard error
+ * Return: 1 after printing the argument
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-        write(2, str1, 59);
-        return (1);
+	char str1[] = "and that piece of art is useful\" - Dora Koprar, 2015-10-19\n";
+	write(2, str1,59);
+	return (1);
 }
