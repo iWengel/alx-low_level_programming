6076ef52a90ce1e05b6144042aa22698e8b4590c
Wengel Awelachew <wenguawelachew@gmail.com>
2022-04-11 05:27:18 -0700

    How to print to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Read the argument
+ * Return: 1 after printing the argument to the standard error
+ */
+int main(void)
+{
+	fprintf( stderr, "and that piece of art is useful" - Dora Koprar, 2015-10-19\n");
+	return (1);
+}
