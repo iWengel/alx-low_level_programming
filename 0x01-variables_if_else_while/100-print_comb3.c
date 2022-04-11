a891637cc0d4fc7397cf36d3a2bcd770df9ec4cc
Wengel Awelachew <wenguawelachew@gmail.com>
2022-04-11 15:26:03 -0700

    Programm to print all possible combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,21 @@ int main(void)
 	{
 		for (b = 48; b < 58; b++)
 		{
-			putchar(a);
-			putchar(b);
-			if (a == 56 && b == 57)
+			while (a < b)
 			{
-				break;
+				if (a == 56 && b == 57)
+				{
+					break;
+				}
+				else
+				{
+					putchar(a);
+					putchar(b);
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			putchar(',');
-			putchar(' ');
-		}
+		}	
 	}
 	putchar('\n');
 	return (0);
