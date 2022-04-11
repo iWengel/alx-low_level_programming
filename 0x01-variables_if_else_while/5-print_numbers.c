dce2d538612a4d408bcb01ab63c40d1d6122a6f5
Wengel Awelachew <wenguawelachew@gmail.com>
2022-04-11 10:51:16 -0700

    Program to print all single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing 0-9
+ */
+int main(void)
+{
+	for (int a = 0; a < 10; a++)
+	{
+		printf("%a", a);
+	}
+	return (0);
+}
