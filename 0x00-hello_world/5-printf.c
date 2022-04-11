e6ee12bc134bedbfc93c2923aa9264193ccd26d0
Wengel Awelachew <wenguawelachew@gmail.com>
2022-04-11 04:38:56 -0700

    How to print characters using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Read the argument
+ *Return: 0 after printing the argument
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
