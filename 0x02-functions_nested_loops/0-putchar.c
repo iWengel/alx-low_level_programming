2865ad7145465ab0accc38f77e8525bad1d7a6dc
iWengel <wenguawelachew@gmail.com>
2022-04-12 06:39:43 -0700

    Program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing _putchar
+ */
+int main(void)
+{
+	putchar(_);
+	putchar(p);
+	putchar(u);
+	putchar(t);
+	putchar(c);
+	putchar(h);
+	putchar(a);
+	putchar(r);
+	putchar('\n');
+	return (0);
+}
+
