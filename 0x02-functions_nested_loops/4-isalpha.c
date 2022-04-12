53247db06adbad126c111087f623e7e37be2f517
iWengel <wenguawelachew@gmail.com>
2022-04-12 08:06:12 -0700

    Programmed function to check if a character is in the alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _isalpha - checks whether a character is an alphabetic character or not
- * @character: contains the argument to be checked
+ * @ch: contains the argument to be checked
  * Return: 1 if true, 0 if false
  */
-int _isalpha(int character)
+int _isalpha(int ch)
 {
-	if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z'))
+	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
 	{
 		return (1);
 	}
