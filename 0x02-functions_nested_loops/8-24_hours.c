3e13ca7b8db4fe9af8ee33cd622de4efc501ae06
iWengel <wenguawelachew@gmail.com>
2022-04-12 10:16:30 -0700

    Programmed function for Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,31 +1,25 @@
 #include "main.h"
-
 /**
- * jack_bauer -prints every minute of the day
+ * jack_bauer -a function that prints every minute of the day
  *
  * Return: Always 0.
  */
 
 void jack_bauer(void)
-
 {
-int a;
-int b;
+	int h;
+	int m;
 
-for (a = 0; a <= 23; a++)
-{
-for (b = 0; b <= 59; b++)
-{
-_putchar (a / 10 + '0');
-_putchar (a % 10 + '0');
-_putchar (':');
-_putchar (b / 10 + '0');
-_putchar (b % 10 + '0');
-_putchar ('\n');
-
-}
-
-
-}
-
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar (h / 10 + '0');
+			_putchar (h % 10 + '0');
+			_putchar (':');
+			_putchar (m / 10 + '0');
+			_putchar (m % 10 + '0');
+			_putchar ('\n');
+		}
+	}
 }
