6e6a8d95f9b9cdf67941930d27484e0910c85a3e
Wengel Awelachew <wenguawelachew@gmail.com>
2022-04-12 09:49:46 -0700

    {rogrammed function to print the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
-*print_last_digit - prints the last digit of a number
-*
-*@n: contains the value to run through the function
-* Return: the last digit of the number
-*/
+ * print_last_digit - prints the last digit of a number
+ * @n: contains the value that runs through the function
+ * Return: the last digit of the number
+ */
 int print_last_digit(int n)
 {
 	int last_digit;
@@ -12,13 +11,13 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		last_digit = (-1 * (n % 10));
-		_putchar (last_digit + '0');
+		_putchar(last_digit + '0');
 		return (last_digit);
 	}
 	else
 	{
 		last_digit = (n % 10);
-		_putchar (last_digit + '0');
+		_putchar(last_digit + '0');
 		return (last_digit);
 	}
-}		
+}
