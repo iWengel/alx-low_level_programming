345075f52c4391ac89d471de77ba0c9ce16848b7
iWengel <wenguawelachew@gmail.com>
2022-04-13 21:40:00 +0300

    Programmed function that prints an nth order multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -44,7 +44,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				}
 			}
-			else if ((c / 100) == 0)
+			else if ((c / 1000) == 0)
 			{
 				_putchar((c / 100) + '0');
 				_putchar(((c / 10) % 10) + '0');
