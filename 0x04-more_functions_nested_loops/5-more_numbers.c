fda517f06e378b45ce0f7ffff77512ab2468a7e8
iWengel <wenguawelachew@gmail.com>
2022-04-14 21:04:26 +0300

    A function that prints the numbers 0-14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,24 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int a;
+	int b;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + '0');
+			if (j < 10)
+			{
+				b = j;
+			}
+			else
+			{
+				a = j / 10;
+				b = j % 10;
+				_putchar(a + '0');
+			}
+			_putchar(b + '0');
 		}
 	}
 	_putchar('\n');
