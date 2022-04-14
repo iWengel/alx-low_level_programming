bfbde6e929fbd65db780331c7db8f4da0baec921
iWengel <wenguawelachew@gmail.com>
2022-04-15 01:38:03 +0300

    A function that prints numbers using only putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * print_number - a function that prints an integer
@@ -27,7 +26,7 @@ void print_number(int n)
 		else if (((n / 100) != 0) && ((n / 1000) == 0))
 		{
 			_putchar((n / 100) +'0');
-			_putcahr(((n / 10) % 10) + '0');
+			_putchar(((n / 10) % 10) + '0');
 			_putchar((n % 10) + '0');
 		}
 		else if (((n / 1000) != 0) && ((n / 10000) == 0))
