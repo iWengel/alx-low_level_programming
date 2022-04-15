410509fd85eeeb78b514b424ee6762d92a32e8db
iWengel <wenguawelachew@gmail.com>
2022-04-15 21:30:41 +0300

    Programmed function that prints an nth order multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,43 +10,32 @@ void print_times_table(int n)
 	int b;
 	int c;
 
-	for (a = 0; a <= n; a++)
+	if (n >= 0 && n <= 15)
 	{
-		for (b = 0; b <= n; b++)
+		for (a = 0; a <= n; a++)
 		{
-			c = a * b;
-			if ((c / 10) == 0)
+			_putchar('0');
+			for (b = 1; b <= n; b++)
 			{
-				if (b == 0)
-				{
-					_putchar('0');
-				}	
-				if (b != 0)
-				{
+				_putchar(',');
+				_putchar(' ');
+				c = a * b;
+				if (c <= 99)
 					_putchar(' ');
+				if (c <= 9)
 					_putchar(' ');
-					_putchar((c % 10) + '0');
+				if (c >= 100)
+				{
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10)) % 10 + '0');
 				}
-				if (b < n)
+				else if (c <= 99 && c >= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar((c / 10) + '0');
 				}
-			}
-			else
-			{
-				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-				if (b < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
 			}
+			_putcahr('\n');
 		}
-		_putchar ('\n');
 	}
 }
-			
