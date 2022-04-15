1840bdaaae4fe2a894d9a626405d477cfba8e600
iWengel <wenguawelachew@gmail.com>
2022-04-15 23:11:57 +0300

    A function that prints an integer using only putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,25 +2,24 @@
 #include <stdio.h>
 /**
  * print_number - prints int with putchar
- * @n: takes number, hello
- *
+ * @n: contains the integer to be printed
  * Return: void
  */
 
 void print_number(int n)
 {
-unsigned int m;
+	unsigned int m;
 
-if (n < 0)
-{
-_putchar('-');
-m = -n;
-}
-else
-{
-m = n;
-}
-if (m / 10 != 0)
-print_number(m / 10);
-_putchar((m % 10) + '0');
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+	else
+	{
+		m = n;
+	}
+	if (m / 10 != 0)
+		print_number(m / 10);
+		_putchar((m % 10) + '0');
 }
