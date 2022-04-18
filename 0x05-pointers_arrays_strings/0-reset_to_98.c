7fb63383813dbfa57f63b8b150f32792c84bdc51
iWengel <wenguawelachew@gmail.com>
2022-04-18 17:45:08 +0300

    Function that changes the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - A function that resets the value ir points to to 98
- * @m: contains the pointer 
+ * @m: contains the pointer
  * Return: nothing.
  */
 void reset_to_98(int *m)
