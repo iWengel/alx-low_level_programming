66209be1ca2a8155e473b16c919e0f513e263247
iWengel <wenguawelachew@gmail.com>
2022-04-18 18:52:52 +0300

    A function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,13 @@
 void rev_string(char *s)
 {
 	int i = 0;
+	char *S;
 
 	while (s[i] != '\0')
 		i++;
-	char *S;
-
 	for (i = i - 1; i >= 0; i--)
 	{
-		S[i] = s[i]
+		S[i] = s[i];
 	}
 	return (S);
 }
