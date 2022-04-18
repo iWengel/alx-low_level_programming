c08e2e1d4226029e7c774f905fa2c10ee3fd00d6
iWengel <wenguawelachew@gmail.com>
2022-04-18 19:05:36 +0300

    Function that prints every other character in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string
+ * @str: a pointer pointing to the string variable
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
