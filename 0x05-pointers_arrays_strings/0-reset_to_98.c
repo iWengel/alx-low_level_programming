ef6edfe8de3c4a52eb605645595527cacfef4314
iWengel <wenguawelachew@gmail.com>
2022-04-18 17:55:02 +0300

    Function that swaps the values of two integers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * reset_to_98 - A function that resets the value ir points to to 98
- * @m: contains the pointer
+ * @n: contains the pointer
  * Return: nothing.
  */
-void reset_to_98(int *m)
+void reset_to_98(int *n)
 {
-	*m = 98;
+	*n = 98;
 }
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - a function that swaps the values of two integers
+ *
+ * Return: Nothing.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
