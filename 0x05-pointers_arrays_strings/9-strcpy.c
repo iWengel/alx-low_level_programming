7d7811faed52ab4d04c2825c289af7a79af0f281
iWengel <wenguawelachew@gmail.com>
2022-04-18 19:36:30 +0300

    Function that copies a string pointed by one pointer to another pointed by another

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,13 +8,14 @@
  */
 char *_strcpy(char *dest, char *scr)
 {
-	int i = 0;
+	int i;
 
-	while (scr[i] != '\0')
+	i = 0;
+	while (src[i] != '\0')
 	{
-		dest[i] = scr[i];
-		i++;
+	dest[i] = src[i];
+	i++;
 	}
-	dest = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
