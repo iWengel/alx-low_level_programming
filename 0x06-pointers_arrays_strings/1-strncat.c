9b6e2ddf596f6fcb898dd59c431758af1aaa6606
iWengel <wenguawelachew@gmail.com>
2022-04-20 21:41:05 +0300

    Function that concatenates two strings with conditons on the second string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - A function that concatenates two strings with conditions on
+ *            the second string
+ * @dest: - first string
+ * @src: - second string
+ * @n: - condition for second string
+ * Return: dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0, b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (b <= n)
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
