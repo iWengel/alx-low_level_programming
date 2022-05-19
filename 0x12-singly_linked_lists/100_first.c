270d84f3641ce262e5a58477c08b405b11723547
iWengel <wenguawelachew@gmail.com>
2022-05-19 18:25:31 +0300

    Function that prints before the main function is executed

--- 0x12-singly_linked_lists/100_first.c
+++ 0x12-singly_linked_lists/100_first.c
@@ -4,7 +4,7 @@
  * bmain - function executed before the main function
  * Return: nothing.
  */
-void __before_main__((constructor)) bmain()
+void __attribute__((constructor)) bmain()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
