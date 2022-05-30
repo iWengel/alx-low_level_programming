220e34e930224f718453c744f9d15036cedd3494
iWengel <wenguawelachew@gmail.com>
2022-05-30 22:04:09 +0300

    Function that reads a text file and prints it to the standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the stdout
+ * read_textfile - reads a text file 
  * @filename: pointer to the text in the file
  * @letters: number of letters
  * 
