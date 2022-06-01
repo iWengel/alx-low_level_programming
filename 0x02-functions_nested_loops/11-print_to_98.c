b31affe130bf777f3468b84c9406886b9d007aa3
iWengel <wenguawelachew@gmail.com>
2022-06-01 16:36:20 +0300

    Function that prints all natural numbers from argument to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,29 +9,22 @@
  */
 void print_to_98(int n)
 {
-	if (n == 98)
-		printf("%d", n);
-	else if (n > 98)
+	if (n < 98)
 	{
-		while (n >= 98)
+		while (n < 98)
 		{
-			printf("%d", n);
-			if (n == 98)
-				break;
-			printf(", ");
-			n--;
+			printf("%d, ", n);
+			n++;
 		}
+		printf("%d\n", n);
 	}
 	else
 	{
-		while (n <= 98)
+		while (n > 98)
 		{
-			printf("%d", n);
-			if (n == 98)
-				break;
-			printf(", ");
-			n++;
+			printf("%d, ", n);
+			n--;
 		}
+		printf("%d\n", n);
 	}
-	_putchar('\n');
 }
