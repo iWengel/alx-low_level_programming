1b63b0914100f897b09fdf021f759be224637f97
iWengel <wenguawelachew@gmail.com>
2022-06-01 19:01:55 +0300

    Program that prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,49 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: 0 after printing the first 98 Fibonacci numbers
+ * Return: Always 0 (Success)
  */
-int main(void)
+int main(void);
 {
+	unsigned long int i = 0, j = 1, sum;
 	int count;
-	unsigned long a = 0, b = 1, sum;
-	unsigned long a1, a2, b1, b2;
-	unsigned long half1, half2;
 
-	for (count = 0; count < 92; count++)
+	count = 0;
+	while (count < 98)
 	{
-		sum = a + b;
-		printf("%lu, ", sum);
-
-		a = b;
-		b = sum;
-	}
-
-	a1 = a / 10000000000;
-	b1 = b / 10000000000;
-	a2 = a % 10000000000;
-	b2 = b % 10000000000;
-
-	for (count = 93; count < 99; count++)
-	{
-		half1 = a1 + b1;
-		half2 = a2 + b2;
-		if (a2 + b2 > 9999999999)
-		{
-			half1 = half1 + 1;
-			half2 = half2 % 10000000000;
-		}
-
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
-			printf(", ");
-
-		a1 = b1;
-		a2 = b2;
-		b1 = half1;
-		b2 = half2;
+		sum = i + j;
+		printf("%lu", sum);
+		if (count == 97)
+			break;
+		printf(", ");
+		i = j;
+		j = sum;
+		count++;
 	}
 	printf("\n");
 	return (0);
--- 0x02-functions_nested_loops/a.c
+++ 0x02-functions_nested_loops/a.c
@@ -3,26 +3,48 @@
 /**
  * main - entry point
  *
- * Return: 0 after printing the sum of even-valued Fibonacci sequence
- *           terms not exceeding 4000000.
+ * Return: 0 after printing the first 98 Fibonacci numbers
  */
 int main(void)
 {
+	int count;
 	unsigned long a = 0, b = 1, sum;
-	float total;
+	unsigned long a1, a2, b1, b2;
+	unsigned long half1, half2;
 
-	while (1)
+	for (count = 0; count < 92; count++)
 	{
 		sum = a + b;
-		if (sum > 4000000)
-			break;
-
-		if ((sum % 2) == 0)
-		total = total + sum;
+		printf("%lu, ", sum);
 
 		a = b;
 		b = sum;
 	}
-	printf("%.0f\n", total);
+
+	a1 = a / 10000000000;
+	b1 = b / 10000000000;
+	a2 = a % 10000000000;
+	b2 = b % 10000000000;
+
+	for (count = 93; count < 99; count++)
+	{
+		half1 = a1 + b1;
+		half2 = a2 + b2;
+		if (a2 + b2 > 9999999999)
+		{
+			half1 = half1 + 1;
+			half2 = half2 % 10000000000;
+		}
+
+		printf("%lu%lu", half1, half2);
+		if (count != 98)
+			printf(", ");
+
+		a1 = b1;
+		a2 = b2;
+		b1 = half1;
+		b2 = half2;
+	}
+	printf("\n");
 	return (0);
 }
