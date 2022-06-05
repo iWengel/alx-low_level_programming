a5c6d4deec844e76be1ad40b6b33ed63a4b54fe0
iWengel <wenguawelachew@gmail.com>
2022-06-05 16:10:01 +0300

    Function that prints a triangle on the terminal

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,7 +18,7 @@ void print_triangle(int size)
 		{
 			for (j = size - 1; j > 0; j--)
 				_putchar(' ');
-			for (k = 0; k < i + 1; k++;)
+			for (k = 0; k < i + 1; k++)
 				_putchar('#');
 			_putchar('\n');
 		}
