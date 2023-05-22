85310d7f3a43ebf4b35851f1077bd6f3d75c294e
Wengel Awelachew <iwengelawelachew@gmail.com>
2023-05-22 19:12:08 +0300

    The alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Program prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+		putchar(i);
+	putchar(\n);
+	return (0);
+}
