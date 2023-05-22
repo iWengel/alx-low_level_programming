243d9f58c2968e53ef0205ff7a3048bcfbab5bf6
Wengel Awelachew <iwengelawelachew@gmail.com>
2023-05-22 09:37:12 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Program prints all lowercase letters except "q" and "e"
+ *
+ * Return: 0 after printing is done
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 || i != 113)
+			putchar(i);
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
