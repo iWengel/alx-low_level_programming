11b47cfc961f8de3ca9022dd027bea1dfbbe7a2d
Wengel Awelachew <iwengelawelachew@gmail.com>
2023-05-31 01:32:34 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Function that prints "_putchar"
+ *
+ * Return: 0 after printing said argument
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to be printed
+ *
+ * Return: 1 (Success)
+ */
+
+int _putchar(char c);
