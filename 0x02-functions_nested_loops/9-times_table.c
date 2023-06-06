945a3dc961c1b395ac146b4f21dec19a5867073c
Wengel Awelachew <iwengelawelachew@gmail.com>
2023-06-06 01:44:17 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return - Nothing.
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			mul = i * j;
+			if (mul < 10)
+				_putchar('0' + (mul));
+			else
+			{
+				_putchar('0' + (mul / 10));
+				_putchar('0' + (mul % 10));
+			}
+			if (j == 9)
+			{
+				_putchar('\n');
+				break;
+			}
+			_putchar(',');
+			_putchar(' ');
+			j++;
+		}
+		i++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -77,4 +77,12 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return - Nothing.
+ */
+
+void times_table(void);
+
 #endif
