eb0faa3ce67666076bbe4d7141edecdc031ec7e9
Wengel Awelachew <iwengelawelachew@gmail.com>
2023-06-07 04:54:44 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -93,4 +93,13 @@ void times_table(void);
 
 int add(int, int);
 
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: argument integer
+ *
+ * Return: Nothing.
+ */
+
+void print_to_98(int n);
+
 #endif
