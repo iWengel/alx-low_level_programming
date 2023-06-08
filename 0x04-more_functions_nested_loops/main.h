65197ec6dc488626a372e6b78ff39f787b205870
Wengel Awelachew <iwengelawelachew@gmail.com>
2023-06-08 03:45:05 -0700

    isdigit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,4 +10,13 @@
 
 int _isupper(int c);
 
+/**
+ * _isdigit - function that checks for a digit
+ * @c: ASCII value of argument character
+ *
+ * Return: 1 if c is digit; 0 otherwise
+ */
+
+int _isdigit(int c);
+
 #endif
