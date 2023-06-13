92827b50b0adb4139349e13806bc1a36318aa228
Wengel Awelachew <iwengelawelachew@gmail.com>
2023-06-13 22:01:48 +0300

    Winning is only half of it. Havinf fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 		length++;
 	if (length % 2 == 0)
 	{
-		for (i = length / 2; i <= length / 2; i++)
+		for (i = length / 2; i <= length; i++)
 			_putchar(str[i]);
 	}
 	else
