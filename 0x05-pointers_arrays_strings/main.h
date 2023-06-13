8ed4a605016fecabcb90911abb71425e3387481f
Wengel Awelachew <iwengelawelachew@gmail.com>
2023-06-13 21:39:53 +0300

    Half the lies they tell about me arenot true

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -66,4 +66,14 @@ void print_rev(char *s);
 
 void rev_string(char *s);
 
+/**
+ * puts2 - function that prints every other character of a string
+ * starting with the first character, followed by a new line.
+ * @str: pointer to the argument string
+ *
+ * Return: Nothing.
+ */
+
+void puts2(char *str);
+
 #endif
