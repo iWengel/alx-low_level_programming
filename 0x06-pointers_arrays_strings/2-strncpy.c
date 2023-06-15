4d34402bc3c39d2fa2d8ad03b5ae1fb6eb151fa7
Wengel Awelachew <iwengelawelachew@gmail.com>
2023-06-15 07:04:50 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,8 +18,7 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i]
 	}
-	for (; dest[i] != '\0'; i++)
-		dest[i] = dest[i];
-	dest[i] = '\0';
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
