3209fcb0afac898bb7348e445864e708a7959314
Wengel Awelachew <iwengelawelachew@gmail.com>
2023-06-19 10:54:45 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -106,4 +106,15 @@ char *rot13(char *);
 
 void print_number(int n);
 
+/**
+ * infinite_add - A function that adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 #endif
